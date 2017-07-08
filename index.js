0ca1b03fb999b077d943134d2a69178b1c581105
Oleksandr Maksymenko <alexandr.maximenko@gmail.com>
2017-07-08 10:58:15 +0300

    Added more controls, grid, and colors

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 //state dimensions
 const stateWidth = 100
-const canvasSize = stateWidth * 5
+let cellWidth = 10
+let canvasSize = stateWidth * cellWidth
+
 const stateSize = stateWidth ** 2
-const initialAliveNum = 10000 //stateSize / 2
-const timeDelay = 500
+const initialAliveNum = stateSize / 2
+let timeDelay = 100
 const initialDeadState = Array.from({length: stateSize}, (_, id) => {
     const cellId = id + 1
 
@@ -48,16 +50,54 @@ const $onScreenCanvas = document.getElementById('canvas')
 $onScreenCanvas.width = canvasSize
 $onScreenCanvas.height = canvasSize
 const onScreenCtx = $onScreenCanvas.getContext('2d')
+onScreenCtx.fillStyle = '#0f0'
 
 const $canvas = document.createElement('canvas')
 $canvas.width = canvasSize
 $canvas.height = canvasSize
 const ctx = $canvas.getContext('2d')
 
-const cellWidth = $canvas.width / stateWidth
+//grid
+const $canvasGrid = document.getElementById('canvas-grid')
+$canvasGrid.width = canvasSize
+$canvasGrid.height = canvasSize
+canvasGridCtx = $canvasGrid.getContext('2d')
+// canvasGridCtx.fillStyle = '#0f0'
+
+const drawGrid = (ctx) => {
+
+  //cellWidth
+  ctx.strokeStyle = '#222'
+  let linesNum = stateWidth + 1
+  let i = 0
+  let x, y
+  ctx.beginPath()
+  for (; i < linesNum; i++) {
+    //vertical lines
+    y = i * cellWidth
+    ctx.moveTo(0, y)
+    ctx.lineTo(canvasSize, y)
+    // ctx.stroke()
+
+    //horizontal lines
+    x = i * cellWidth
+    ctx.moveTo(x, 0)
+    ctx.lineTo(x, canvasSize)
+  }
+  ctx.stroke()
+}
+
+
+const updateCanvasSize = (canvasSize) => {
+  // $onScreenCanvas.width = canvasSize
+  // $onScreenCanvas.height = canvasSize
+  // $canvas.width = canvasSize
+  // $canvas.height = canvasSize
+}
+
 
 const clearCanvas = (ctx) => {
-  ctx.clearRect(0, 0, $canvas.width, $canvas.height)
+  ctx.clearRect(0, 0, canvasSize, canvasSize)
 }
 
 const renderCell = ({id}, ctx) => {
@@ -65,19 +105,19 @@ const renderCell = ({id}, ctx) => {
   const x = id - (y - 1) * stateWidth
   ctx.fillRect((x - 1) * cellWidth, (y - 1) * cellWidth, cellWidth, cellWidth)
 }
-
-const renderState = state => {
-  clearCanvas(ctx)
-  clearCanvas(onScreenCtx)
-
-  state.forEach(cell => {
-    if (cell.alive) {
-      renderCell(cell, ctx)
-    }
-  })
-
-  onScreenCtx.drawImage($canvas, 0, 0)
-}
+//
+// const renderState = state => {
+//   clearCanvas(ctx)
+//   clearCanvas(onScreenCtx)
+//
+//   state.forEach(cell => {
+//     if (cell.alive) {
+//       renderCell(cell, ctx)
+//     }
+//   })
+//
+//   onScreenCtx.drawImage($canvas, 0, 0)
+// }
 
 //render counter
 const $counter = document.getElementById('counter')
@@ -108,8 +148,13 @@ const generateAliveCells = (state, aliveQty) => {
 
 
 const updateStateOnce = state => {
-    clearCanvas(ctx)
-    clearCanvas(onScreenCtx)
+  // clearCanvas(ctx)
+  // clearCanvas(onScreenCtx)
+  ctx.rect(0, 0, canvasSize, canvasSize)
+  ctx.fillStyle = '#000'
+  ctx.fill()
+  ctx.fillStyle = '#0f0'
+  // drawGrid()
 
 
   const getAliveNeighborsNum = (neighborsIds) => {
@@ -171,6 +216,7 @@ const initializeGame = () => {
   startTime = Date.now()
   counter = 1
   gameIsRunning = true
+  drawGrid(canvasGridCtx)
   console.log("initialized...")
 }
 
@@ -189,16 +235,16 @@ const runGame = () => {
   }
   renderCounter(++counter)
 
-  if (!gameIsRunning) {
-    console.log('gameIsRunning', gameIsRunning)
-    clearInterval(intervalId)
-  }
+  // if (!gameIsRunning) {
+  //   console.log('gameIsRunning', gameIsRunning)
+  //   clearInterval(intervalId)
+  // }
 
-  if (allDead) {
-    console.log('allDead', allDead)
-    clearInterval(intervalId)
-    clearCanvas(onScreenCtx)
-  }
+  // if (allDead) {
+  //   console.log('allDead', allDead)
+  //   clearInterval(intervalId)
+  //   clearCanvas(onScreenCtx)
+  // }
 }
 
 let intervalId
@@ -208,8 +254,9 @@ const handlerOnStartGame = () => {
   $btnStop.style.display = "inline-block"
   $btnPause.style.display = "inline-block"
   $btnRestart.style.display = "inline-block"
+  $btnKill.style.display = "inline-block"
   initializeGame()
-  intervalId = setInterval(runGame, intervalId)
+  intervalId = setInterval(runGame, timeDelay)
   console.log('started...')
 }
 const handlerOnStopGame = () => {
@@ -246,13 +293,94 @@ const handlerOnRestartGame = () => {
   intervalId = setInterval(runGame, timeDelay)
 }
 
+const handlerOnKill = () => {
+  console.log("killing")
+  killAllCells(state)
+}
+
+const handlerOnZoomIn = () => {
+  cellWidth++
+  canvasSize = stateWidth * cellWidth
+  // updateCanvasSize(canvasSize)
+
+}
+
+const handlerOnZoomOut = () => {
+  cellWidth--
+  canvasSize = stateWidth * cellWidth
+  // updateCanvasSize(canvasSize)
+}
+
+const handlerOnSpeedUp = () => {
+  handlerOnPauseGame()
+  timeDelay -= 10
+  handlerOnPauseGame()
+}
+
+const handlerOnSpeedDown = () => {
+  handlerOnPauseGame()
+  timeDelay += 10
+  handlerOnPauseGame()
+}
+
 //controls
 const $btnStart = document.getElementById('btnStart')
 const $btnStop = document.getElementById('btnStop')
 const $btnPause = document.getElementById('btnPause')
 const $btnRestart = document.getElementById('btnRestart')
+const $btnKill = document.getElementById('btnKill')
+const $btnZoomIn = document.getElementById('btnZoomIn')
+const $btnZoomOut = document.getElementById('btnZoomOut')
+const $btnSpeedUp = document.getElementById('btnSpeedUp')
+const $btnSpeedDown = document.getElementById('btnSpeedDown')
 
 $btnStart.addEventListener('click', handlerOnStartGame)
 $btnStop.addEventListener('click', handlerOnStopGame)
 $btnPause.addEventListener('click', handlerOnPauseGame)
 $btnRestart.addEventListener('click', handlerOnRestartGame)
+$btnKill.addEventListener('click', handlerOnKill)
+$btnZoomIn.addEventListener('click', handlerOnZoomIn)
+$btnZoomOut.addEventListener('click', handlerOnZoomOut)
+$btnSpeedUp.addEventListener('click', handlerOnSpeedUp)
+$btnSpeedDown.addEventListener('click', handlerOnSpeedDown)
+
+
+const handlerOnToggleCell = (e) => {
+  const $onScreenCanvasRect = $onScreenCanvas.getBoundingClientRect()
+  const left = $onScreenCanvasRect.left
+  const top = $onScreenCanvasRect.top
+  const right = $onScreenCanvasRect.right
+  const bottom = $onScreenCanvasRect.bottom
+  const clientX = e.clientX
+  const clientY = e.clientY
+
+  if (clientX < left || clientX > right || clientY < top || clientY > bottom) {
+    console.log(clientX, left, right, clientY, top, bottom)
+    return
+  }
+  // console.log("clicked", left, top, right, bottom, clientY, clientX)
+  const x = clientX - left
+  const y = clientY - top
+  const newX = Math.ceil(x / cellWidth)
+  const newY = Math.ceil(y / cellWidth)
+  const cellId = newX + (newY - 1) * stateWidth
+  console.log(newX, newY, cellId)
+  let clickedCell = state.find(cell => cell.id === cellId)
+  console.log(clickedCell)
+  clickedCell.alive = !clickedCell.alive
+  if (clickedCell.alive) {
+    onScreenCtx.fillRect((newX - 1) * cellWidth, (newY - 1) * cellWidth, cellWidth, cellWidth)
+    // renderCell(clickedCell, onScreenCtx)
+  }
+  else if (!clickedCell.alive) {
+    onScreenCtx.fillStyle = '#000'
+    onScreenCtx.fillRect((newX - 1) * cellWidth, (newY - 1) * cellWidth, cellWidth, cellWidth)
+    onScreenCtx.fillStyle = '#0f0'
+  }
+  console.log(clickedCell.alive)
+
+}
+
+document.addEventListener('click', handlerOnToggleCell)
+
+
